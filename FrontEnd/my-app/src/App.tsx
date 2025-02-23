4e172ccf03d638daf9bb7d33c12e7100344d34e6
Marcos Vinicius <mvsenko7@gmail.com>
2025-02-23 20:43:58 -0300

    iniciando o desenvolvimento com cards frontend

--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -4,13 +4,18 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="">
-      Aula 2 - Componentes
-      <Card
-        texto="Meu card com props"
-        header="Card 1"
-        footer="Footer card 1"
-      ></Card>
-      <Card texto="Meu segundo card com prosp" header="Header Card 2"></Card>
+      <header>
+        <p>The Barber Man</p>
+        <div className=""></div>
+      </header>
+      <div className="cards">
+        <Card
+          texto="Meu card com props"
+          header="Barbeiro 1"
+          footer="Footer card 1"
+        ></Card>
+        <Card texto="Meu segundo card com prosp" header="Barbeiro 2"></Card>
+      </div>
     </div>
   );
 }
