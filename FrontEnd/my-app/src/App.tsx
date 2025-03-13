2e9e722d176259b58a7ca375f77c7192f285f0c1
Marcos Vinicius <mvsenko7@gmail.com>
2025-03-13 19:03:42 -0300

    mudanca de tema: Començando agora ECOMMERCE. Aqui, está sendo lancado header, card e nav

--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -1,24 +1,16 @@
-import "./App.css";
-import Card from "./components/Card";
+import Header from "./componentes/Header";
+import "./styles/Styles.css";
+import Card from "./componentes/Card";
+import Nav from "./componentes/Nav";
+import "./styles/Card.css";
+import "./styles/Nav.css";
 
 function App() {
   return (
-    <div className="">
-      <header>
-        <p>The Barber Man</p>
-        <div className="div-button">
-          <button className="button-login">Login</button>
-          <button className="button-registro">Registre-se</button>
-        </div>
-      </header>
-      <div className="cards">
-        <Card
-          texto="Meu card com props"
-          header="Barbeiro 1"
-          footer="Footer card 1"
-        ></Card>
-        <Card texto="Meu segundo card com prosp" header="Barbeiro 2"></Card>
-      </div>
+    <div className="app">
+      <Header></Header>
+      <Nav></Nav>
+      <Card></Card>
     </div>
   );
 }
--- FrontEnd/my-app/src/components/Card.tsx
+++ FrontEnd/my-app/src/components/Card.tsx
@@ -1,21 +1,116 @@
-import "./Card.css";
-
-type CardProps = {
-  texto: string;
-  header?: string;
-  footer?: string;
-};
-
-const Card = ({ texto, header, footer }: CardProps) => {
+function Card() {
   return (
-    <div className="container">
-      <div className="header">{header}</div>
-      <div className="body">{texto}</div>
-      {footer ? <div className="footer">{footer}</div> : <></>}
+    <div className="lista-cards">
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
+      <div className="produto">
+        <img src="src/img/user.png"></img>
+        <h2>NOME PRODUTO</h2>
+        <p>Descrição</p>
+        <div className="button-card">
+          <button className="button-carrinho">Adicionar ao carrinho</button>
+          <button className="button-comprar">Comprar</button>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Card;
-
-//slr
