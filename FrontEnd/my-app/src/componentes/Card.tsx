172164c7bd2198520ef992061825675237755fe4
Marcos Vinicius <mvsenko7@gmail.com>
2025-03-14 08:59:39 -0300

    file update

---
+++ FrontEnd/my-app/src/componentes/Card.tsx
@@ -0,0 +1,116 @@
+function Card() {
+    return(
+        <div className="lista-cards">
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                    <button className="button-carrinho">Adicionar ao carrinho</button>
+                    <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+            <div className="produto">
+                <img src="src/img/user.png"></img>
+                <h2>NOME PRODUTO</h2>
+                <p>Descrição</p>
+                <div className="button-card">
+                <button className="button-carrinho">Adicionar ao carrinho</button>
+                <button className="button-comprar">Comprar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ FrontEnd/my-app/src/componentes/Header.tsx
@@ -0,0 +1,24 @@
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <div>
+        <header>
+          <div className="titulo-principal">
+              <h1>GYM</h1>
+              <h3>POISON</h3>
+          </div>
+           <div className="div-usuario">
+              <div className="div-login">
+                <button>Login</button>
+              </div>
+              <div className="div-registro">
+                <button>Registre-se</button>
+              </div>
+           </div>
+        </header>
+    </div>
+  );
+}
+
+export default Header;
