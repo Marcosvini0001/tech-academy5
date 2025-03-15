0de84b7edac9a2bc491cf7e8d4f6474325d82688
Marcos Vinicius <mvsenko7@gmail.com>
2025-03-14 22:17:52 -0300

    hover button login-register

--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -1,18 +1,19 @@
-import Header from './componentes/Header'
-import './styles/Styles.css';
-import Card from'./componentes/Card'
-import Nav from './componentes/Nav'
-import './styles/Card.css'
-import './styles/Nav.css'
+import Header from "./componentes/Header";
+import "./styles/Styles.css";
+import Card from "./componentes/Card";
+import Nav from "./componentes/Nav";
+import "./styles/Card.css";
+import "./styles/Nav.css";
+import Footer from "./componentes/Footer";
+import "./styles/Footer.css";
 
 function App() {
-
   return (
     <div className="app">
       <Header></Header>
       <Nav></Nav>
       <Card></Card>
-
+      <Footer></Footer>
     </div>
   );
 }
