8b1a970dadd47dc973f6e195fd7d5ca3b14bd012
Marcos Vinicius <mvsenko7@gmail.com>
2025-03-22 19:01:05 -0300

    add fetch

--- BackEnd/src/models/produtoModel.ts
+++ BackEnd/src/models/produtoModel.ts
@@ -36,7 +36,7 @@ ProdutoModel.init(
       allowNull: false,
     },
     preco: {
-      type: DataTypes.STRING,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
     },
     descricao: {
--- FrontEnd/my-app/src/componentes/Card.tsx
+++ FrontEnd/my-app/src/componentes/Card.tsx
@@ -1,15 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Card() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/produtos")
+      .then((response) => {
+        console.log("Dados recebidos da API:", response.data);
+        setProdutos(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar produtos:", error);
+      });
+  }, []);
+
   return (
     <div className="lista-cards">
-      <div className="produto">
-        <img src="src/img/user.png"></img>
-        <h2>NOME PRODUTO</h2>
-        <p>Descrição</p>
-        <div className="button-card">
-          <button className="button-carrinho">Adicionar ao carrinho</button>
-          <button className="button-comprar">Comprar</button>
+      {produtos.map((produto) => (
+        <div className="produto" key={produto.id}>
+          <img src="src/img/user.png" alt={produto.name} />
+          <h2>{produto.name}</h2>
+          <p>{produto.descricao}</p>
+          <p>
+            <strong>Preço:</strong> R$ {produto.preco}
+          </p>
+          <div className="button-card">
+            <button className="button-carrinho">Adicionar ao carrinho</button>
+            <button className="button-comprar">Comprar</button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
