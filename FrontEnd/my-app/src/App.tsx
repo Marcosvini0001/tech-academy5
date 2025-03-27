0c207c88f1832c9fe2e7d1d9c910d320154c6f2d
Marcos Vinicius <mvsenko7@gmail.com>
2025-03-27 18:25:20 -0300

    starting for cadastroProdutos

--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -7,9 +7,10 @@ import Home from "./pages/Home";
 import Footer from "./componentes/Footer";
 import Login from "./pages/Login";
 import Registro from "./pages/Registro";
-
+import CadastroProdutos from "./pages/CadastroProdutos";
 import "../src/styles/Login.css";
 import "../src/styles/Registro.css";
+
 function App() {
   return (
     <Router>
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Registro />} />
+          <Route path="/cadastrop" element={<CadastroProdutos />} />
         </Routes>
         <Card />
         <Footer />
---
+++ FrontEnd/my-app/src/pages/CadastroProdutos.tsx
@@ -0,0 +1,106 @@
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+
+const CadastroProdutos = () => {
+  const [name, setName] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [marca, setMarca] = useState("");
+  const [preco, setPreco] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3000/produtos", {
+        name,
+        categoria,
+        marca,
+        preco,
+        descricao,
+      });
+
+      console.log("Usuário registrado:", response.data);
+      alert("Usuário registrado com sucesso!");
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data?.message || "Erro ao registrar usuário");
+      } else {
+        setError("Erro desconhecido ao registrar usuário");
+      }
+    }
+  };
+
+  return (
+    <div className="registro">
+      <h1>Cadastrar Produtos</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form className="div-dados" onSubmit={handleSubmit}>
+        <label htmlFor="nome">Nome do produto:</label>
+        <input
+          type="text"
+          id="nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Digite nome do produto"
+        />
+        <br />
+        <label htmlFor="cpf">Categoria:</label>
+        <input
+          type="text"
+          id="categoria"
+          value={categoria}
+          onChange={(e) => setCategoria(e.target.value)}
+          required
+          placeholder="Digite a categoria"
+        />
+        <br />
+        <label htmlFor="email">Marca:</label>
+        <input
+          type="text"
+          id="marca"
+          value={marca}
+          onChange={(e) => setMarca(e.target.value)}
+          required
+          placeholder="Marca do produto"
+        />
+        <br />
+        <label htmlFor="endereco">Preco:</label>
+        <input
+          type="text"
+          id="preco"
+          value={preco}
+          onChange={(e) => setPreco(e.target.value)}
+          required
+          placeholder="Preço do produto"
+        />
+        <br />
+        <label htmlFor="cep">Descrição:</label>
+        <input
+          type="text"
+          id="descricao"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          required
+          placeholder="Descrição"
+        />
+        <br />
+        <div className="buttons">
+          <div className="link-home">
+            <Link id="link-button" to="/">
+              Ir para o Home
+            </Link>
+          </div>
+          <div className="registrar">
+            <button type="submit">Cadastrar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CadastroProdutos;
